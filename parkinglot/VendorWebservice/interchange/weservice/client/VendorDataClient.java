961ab2230fdcce2918b2ea8ef445833524012e34
bkumaresan <bkumaresan@ithaka.org>
2011-12-21 20:36:08 +0000

    modified

--- parkinglot/VendorWebservice/interchange/weservice/client/VendorDataClient.java
+++ parkinglot/VendorWebservice/interchange/weservice/client/VendorDataClient.java
@@ -1,27 +1,28 @@
 package org.ithaka.cm.ebooks.vendor.interchange.weservice.client;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.ithaka.cm.ebooks.vendor.interchange.webservice.IthakaService;
 import org.ithaka.cm.ebooks.vendor.interchange.webservice.IthakaServiceInteface;
 import org.ithaka.cm.ebooks.vendor.interchange.webservice.IthakaServiceService;
 
 public class VendorDataClient {
+	private static Log logger =LogFactory.getLog(VendorDataClient.class);
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		VendorDataWSSSLClient client =new VendorDataWSSSLClient();
 		String response=null;
 		try {
 			response = client.getResponse("vendor1");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		logger.error("Exception while posting the request "+e);
 		}
-		System.out.println(response);
-//		IthakaServiceService se = new IthakaServiceService();
-//		IthakaServiceInteface in=se.getIthakaServicePort();
-//		System.out.println(in.sayHello("Bala"));
+		logger.info((response));
+
 		
 
 	}
