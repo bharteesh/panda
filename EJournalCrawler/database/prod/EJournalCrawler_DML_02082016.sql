0b85110a2b90ad6177acf8b4024c61991ce7b83d
Suresh Kadirvel <Suresh.Kadirvel@ithaka.org>
2016-02-08 17:57:43 +0000

    PANDA-339 - Corrections and additions

--- EJournalCrawler/database/prod/EJournalCrawler_DML_02082016.sql
+++ EJournalCrawler/database/prod/EJournalCrawler_DML_02082016.sql
@@ -11,19 +11,16 @@ Insert into CRAWLER_CONFIG
 	(3,'CJPH','/cmjp/EJournalCrawler/Heritrix/Software/heritrix-3.2.0/jobs/canajpublheal/latest/warcs','/cmjp/EJournalCrawler/Content','http://journal.cpha.ca/index.php/cjph/issue/archive',
 	'jstor@aa2ptcthumper05.ithaka.org:/csp_content/ongoing/backfileCSP/PAGE_IMAGES','Y');	
 
-
-ALTER TABLE CRAWLER_CONFIG ADD EXTRACTOR_NAME VARCHAR2(255);
-ALTER TABLE CRAWLER_CONFIG ADD JOURNAL_CODE VARCHAR2(255);
-ALTER TABLE CRAWLER_CONFIG ADD IS_ACTIVE CHAR(1);
-
 UPDATE CRAWLER_CONFIG SET EXTRACTOR_NAME='MLRPDFExtractor' WHERE PROVIDER='MLR';
 UPDATE CRAWLER_CONFIG SET EXTRACTOR_NAME='CJSPDFExtractor' WHERE PROVIDER='CJS';
 UPDATE CRAWLER_CONFIG SET EXTRACTOR_NAME='CJSPDFExtractor' WHERE PROVIDER='CJPH';
 
-UPDATE CRAWLER_CONFIG SET JOURNAL_CODE='monthlylaborreview' WHERE PROVIDER='MLR';
+UPDATE CRAWLER_CONFIG SET JOURNAL_CODE='monthlylaborrev' WHERE PROVIDER='MLR';
 UPDATE CRAWLER_CONFIG SET JOURNAL_CODE='canajsocicahican' WHERE PROVIDER='CJS';
 UPDATE CRAWLER_CONFIG SET JOURNAL_CODE='canajpublheal' WHERE PROVIDER='CJPH';
 
-UPDATE CRAWLER_CONFIG SET IS_ACTIVE='Y';
+update CRAWLER_CONFIG set start_year=2008, start_volume=33, start_issue=1 where JOURNAL_CODE='canajsocicahican'
+update CRAWLER_CONFIG set start_year=2012,  start_volume=103, start_issue=4  where JOURNAL_CODE='canajpublheal'
+
 
 COMMIT;
